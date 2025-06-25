8178f893a6c2ea6f109ca7e796ac5aae6063b113
kfields <kurtisfields@gmail.com>
2025-06-24 21:41:08 -0600

    fix camera clamping

--- badwing/screens/tile_level_screen.py
+++ badwing/screens/tile_level_screen.py
@@ -33,75 +33,42 @@ class TileLevelScreen(LevelScreen):
         if self.avatar is None:
             return
         self.camera.position = self.avatar.position
-        self.update_camera(force_changed=True)
+        self.update_camera()
         
     def update(self, delta_time: float):
         self.update_camera(delta_time)
         super().update(delta_time)
 
-    def update_camera(self, delta_time: float = 1/60, force_changed: bool = False):
+    def update_camera(self, delta_time: float = 1/60):
         if self.avatar is None:
             return
-        # --- Manage Scrolling ---
-        # Track if we need to change the viewport
-        changed = False
-        # Scroll left
-        left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
-        if self.avatar.bounds.left < left_boundary or force_changed:
-            self.view_left -= left_boundary - self.avatar.bounds.left
-            changed = True
+        velocity = self.avatar.velocity
+        speed = glm.length(velocity) / 128
 
-        # Scroll right
-        right_boundary = self.view_left + self.width - RIGHT_VIEWPORT_MARGIN
-        if self.avatar.bounds.right > right_boundary  or force_changed:
-            self.view_left += self.avatar.bounds.right - right_boundary
-            changed = True
+        #logger.debug(f"delta_time: {delta_time}")
+        #logger.debug(f"velocity: {velocity}")
+        #logger.debug(f"speed: {speed}")
 
-        # Scroll up
-        top_boundary = self.view_bottom + self.height - TOP_VIEWPORT_MARGIN
-        if self.avatar.bounds.top > top_boundary or force_changed:
-            self.view_bottom += self.avatar.bounds.top - top_boundary
-            changed = True
+        frustum = self.camera.frustum
 
-        # Scroll down
-        bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
-        if self.avatar.bounds.bottom < bottom_boundary or force_changed:
-            self.view_bottom -= bottom_boundary - self.avatar.bounds.bottom
-            changed = True
+        # Compute clamping limits to keep the frustum inside level bounds
+        min_x = self.scene.bounds.min.x + (frustum.max.x - frustum.min.x) / 2
+        max_x = self.scene.bounds.max.x - (frustum.max.x - frustum.min.x) / 2
+        min_y = self.scene.bounds.min.y + (frustum.min.y - frustum.max.y) / 2
+        max_y = self.scene.bounds.max.y - (frustum.min.y - frustum.max.y) / 2
 
-        if changed:
-            velocity = self.avatar.velocity
-            #speed = glm.length(velocity) / 256
-            speed = glm.length(velocity) / 128
+        #min_y = max(self.scene.bounds.min.y, min_y)
+        # TODO: Hack.
+        min_y = min_y + frustum.height
 
-            #logger.debug(f"delta_time: {delta_time}")
-            #logger.debug(f"velocity: {velocity}")
-            #logger.debug(f"speed: {speed}")
+        #logger.debug(f"scene bounds: {self.scene.bounds}")
+        #logger.debug(f"frustum: {frustum}")
+        #logger.debug(f"min_x: {min_x}")
+        #logger.debug(f"max_x: {max_x}")
+        #logger.debug(f"min_y: {min_y}")
+        #logger.debug(f"max_y: {max_y}")
 
-            frustrum = self.camera.frustrum
-
-            # Compute clamping limits to keep the frustum inside level bounds
-            min_x = self.scene.bounds.min.x + (frustrum.max.x - frustrum.min.x) / 2
-            max_x = self.scene.bounds.max.x - (frustrum.max.x - frustrum.min.x) / 2
-            min_y = self.scene.bounds.min.y + (frustrum.min.y - frustrum.max.y) / 2
-            max_y = self.scene.bounds.max.y - (frustrum.min.y - frustrum.max.y) / 2
-
-            #min_y = max(self.scene.bounds.min.y, min_y)
-            # TODO: Hack.
-            #min_y = min_y + frustrum.height - 128
-            min_y = min_y + frustrum.height
-            #max_y = min(self.scene.bounds.max.y, max_y)
-            #max_y = max_y + frustrum.height
-
-            #logger.debug(f"scene bounds: {self.scene.bounds}")
-            #logger.debug(f"frustrum: {frustrum}")
-            #logger.debug(f"min_x: {min_x}")
-            #logger.debug(f"max_x: {max_x}")
-            #logger.debug(f"min_y: {min_y}")
-            #logger.debug(f"max_y: {max_y}")
-
-            camera_position = glm.mix(self.camera.position, self.avatar.position, speed * delta_time)
-            camera_x = glm.clamp(camera_position.x, min_x, max_x)
-            camera_y = glm.clamp(camera_position.y, min_y, max_y)
-            self.camera.position = glm.vec2(camera_x, camera_y)
-
+        camera_position = glm.mix(self.camera.position, self.avatar.position, speed * delta_time)
+        camera_x = glm.clamp(camera_position.x, min_x, max_x)
+        camera_y = glm.clamp(camera_position.y, min_y, max_y)
+        self.camera.position = glm.vec2(camera_x, camera_y)
